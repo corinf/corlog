d0ff68f71f9c4d66039b3135a1c21cffeba935b8
snowmas <snowmas@users.noreply.github.com>
2020-10-26 00:02:30 +0100

    added /templates/ folder for blog.yml

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("C:\\dev\\corlog\\.cache\\caches\\gatsby-plugin-offline\\app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\dev\\corlog\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\dev\\corlog\\src\\pages\\404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("C:\\dev\\corlog\\src\\pages\\about.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\dev\\corlog\\src\\pages\\index.js"))),
